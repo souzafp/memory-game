ecdb2ba0503e32314ccd2639a78a3f9b21232634
Filipe Souza <filipe10souza@gmail.com>
2021-05-31 20:41:30 -0300

    Initial Commit

---
+++ memory-game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  memory-game
+//
+//  Created by Filipe Souza on 31/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ memory-game/memory_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  memory_gameApp.swift
+//  memory-game
+//
+//  Created by Filipe Souza on 31/05/21.
+//
+
+import SwiftUI
+
+@main
+struct memory_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
