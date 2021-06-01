f801be89825576f4833ff0f49835760f963bac5c
Filipe Souza <filipe10souza@gmail.com>
2021-05-31 21:05:06 -0300

    Initial Commit

--- memory-game/ContentView.swift
+++ memory-game/ContentView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    var body: some View{
+        HStack{
+            ForEach(0..<4){ index in
+                CardView(isFaceUp: true)
+            }
+    }
+        .padding()
+        .foregroundColor(.orange)
+        .font(.largeTitle)
+    }
+}
+
+struct CardView: View{
+    var isFaceUp: Bool
+    var body: some View{
+        ZStack{
+            if isFaceUp{
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3.0)
+                Text("⚽️")
+            } else{
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
     }
 }
 
